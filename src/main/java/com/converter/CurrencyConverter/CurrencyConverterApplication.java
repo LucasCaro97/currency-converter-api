096889a73b94912414c3625cd7c5467f2e86e101
lucas <lucascaro97@gmail.com>
2024-01-05 11:29:26 -0300

    change cors config

--- src/main/java/com/converter/CurrencyConverter/CurrencyConverterApplication.java
+++ src/main/java/com/converter/CurrencyConverter/CurrencyConverterApplication.java
@@ -2,6 +2,9 @@ package com.converter.CurrencyConverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
@@ -10,4 +13,14 @@ public class CurrencyConverterApplication {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://200.58.107.39:5173").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
